b5eff35939361f3e503b294b6d01264e3e61cecd
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-08-01 00:22:26 +0900

    0801 Update boardList page
    Update files
    
    css, form

--- client/src/components/views/Board/BoardList.js
+++ client/src/components/views/Board/BoardList.js
@@ -3,36 +3,61 @@ import '../Board/BoardList.css'
 
 function BoardList(props) {
     return (
+        <section className="boardList-section border">
+            <article>
+                <div className="boardList-container">
+                    <table className="BoardTable">
+                        <thead className=" ">
+                            <tr className="">
+                                <th>번호</th>
+                                <th>제목</th>
+                                <th>작성자</th>
+                                <th>작성날짜</th>
+                                <th>조회 수</th>
+                                <th>추천 수</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Title</td>
+                                <td>writer</td>
+                                <td>20-07-31</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Title</td>
+                                <td>writer</td>
+                                <td>20-07-31</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Title</td>
+                                <td>writer</td>
+                                <td>20-07-31</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Title</td>
+                                <td>writer</td>
+                                <td>20-07-31</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
 
-        <div className="app">
-            <h3>BoardList</h3>
-            <div className="boardList-container">
-                
-                <table className="border test BoardTable">
-                    <thead className="border flex">
-                        <tr className="flex">
-                            <th>번호</th>
-                            <th>제목</th>                            
-                            <th>작성자</th>                            
-                            <th>작성날짜</th>                            
-                            <th>조회 수</th>                            
-                            <th>추천 수</th>                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <td>1</td>
-                        <td>Title</td>
-                        <td>writer</td>
-                        <td>20-07-31</td>
-                        <td>0</td>
-                        <td>0</td>
-                    </tbody>
-                    <tfoot>
-
-                    </tfoot>
-                </table>
-            </div>
-        </div>
+                        </tfoot>
+                    </table>
+                </div>
+            </article>
+        </section>
     )
 }
 
