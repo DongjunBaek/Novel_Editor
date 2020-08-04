6269e3aacd6992c481d3ec2082c2554b09caa5c7
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-08-04 23:00:54 +0900

    Update fils boardDetail.css
    게시판 상세보기 페이지 수정
    
    테스트 용 폼으로 제작
    나중에 수정할 예정

--- client/src/components/views/Board/BoardDetail/BoardDetail.js
+++ client/src/components/views/Board/BoardDetail/BoardDetail.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import '../BoardDetail/BoardDetail.css';
-
+// BOARD_DETAILPAGE
 function BoardDetail() {
     return (
         <section className="BoardDetail-section">
-            <h1>BoardDetail</h1>
-            <article className="BoardDetail-artcle">
+            
+            <article className="BoardDetail-artcle test">
                 {/* 번호, 제목, 작성자, 작성날짜, 조회 수, 추천 수  */}
-                <div className="board-container">
-                    <div className="board-sideBar">
-                        <ul>
+                <div className="board-container test">
+                    <div className="board-sideBar test">
+                        <ul className="board-sideBar-ul test">
                             <li>목록으로가기</li>
                             <li>즐겨찾기</li>
                             <li>맨위로</li>
@@ -18,20 +18,25 @@ function BoardDetail() {
                         </ul>
                     </div>
 
-                    <div className="board-content">
-                        <div className="board-info">
-                            <ul>
-                                <li>글 번호</li>                                
+                    <div className="board-content test">
+                        <div className="board-info test">
+                            <ul class="board-info-ul test">
+                                <li>글 번호</li>
+                                <li></li>                                
                                 <li>작성자</li>
+                                <li></li>                                
                                 <li>작성 날짜</li>
+                                <li></li>                                
                                 <li>조회 수</li>
+                                <li></li>                                
                                 <li>추천 수</li>
+                                <li></li>                                
                             </ul>
                         </div>
-                        <div className="board-title">
+                        <div className="board-title test">
                             <p>글 제목</p>
                         </div>
-                        <div className="board-text">
+                        <div className="board-text test">
                             <p>글 내용</p>
                         </div>
 
