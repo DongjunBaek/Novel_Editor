a1a264089796b257eb0c5d711241107d5940a42e
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-08-05 22:31:31 +0900

    20200805_novelEditor_구조만들기
    0805
    
    모델 객체 구성 및 페이지 구성 추가

---
+++ client/src/components/views/Novel/NovelList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideMenu from './Sections/SideMenu';
+
+function NovelList() {
+    return (
+        <div>
+            <SideMenu />
+            NovelList
+        </div>
+    )
+}
+
+export default NovelList
--- server/models/Board.js
+++ server/models/Board.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 
-// 작성날짜 2020 08 03
+// 작성날짜 2020 08 05
 // author : GGwin
-// 설명 소설 스키마
+// info   : Board 
 // field
 // 글번호, 글제목, 글내용, 조회수, 좋아요수, 싫어요수, 별점, 작성자, 비고, 댓글
-const novelSchema = mongoose.Schema({
+const boardSchema = mongoose.Schema({
     no :{
         type : Number,
         unique : 1
@@ -25,14 +25,6 @@ const novelSchema = mongoose.Schema({
         type : Number,
         default : 0
     },
-    unLike : {
-        type : Number,
-        default : 0
-    },
-    star : {
-        type : Number,
-        default : 0
-    },
     author : {
         type : String
     },
@@ -45,17 +37,10 @@ const novelSchema = mongoose.Schema({
     images : {
         type : Array,
         default : []
-    },
-    characters : {
-        type : Object,
-        default : {}
     }
-
-
-
 })
 
 
-const Novel = mongoose.model('Novel', novelSchema);
+const Board = mongoose.model('Board', boardSchema);
 
-module.exports = { Novel }
+module.exports = { Board }
