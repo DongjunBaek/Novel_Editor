41239d6ad29e76445083531dd208eb461f116669
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-08-09 22:59:16 +0900

    20200809_novelEditor_구조만들기
    소설 상세보기페이지
    
    구조작성중

--- client/src/components/views/Novel/NovelDetail/NovelDetail.js
+++ client/src/components/views/Novel/NovelDetail/NovelDetail.js
@@ -7,7 +7,27 @@ function NovelDetail() {
         <section className="NovelDetail-container">
             <NovelMenu />
             <article className="Novel-article test">
-                novelarticle
+                <div className="Novel-info">
+                    <table className="Novel-info-ul" >
+                        <tr>
+                            <td>제목</td>
+                            <td>title</td>
+                            <td>작성자</td>
+                            <td>writer</td>
+                            <td>별점</td>
+                            <td>0.0</td>
+                            <td>추천</td>
+                            <td>1</td>
+                            <td>조회수</td>
+                            <td>1</td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div className="Novel-content">
+                    <p>소설 내용</p>
+                </div>
+
             </article>
         </section>
     )
--- client/src/components/views/Novel/NovelDetail/Sections/NovelMenu.js
+++ client/src/components/views/Novel/NovelDetail/Sections/NovelMenu.js
@@ -4,7 +4,13 @@ import '../Sections/NovelMenu.css'
 function NovelMenu() {
     return (
         <article className="novelMenu-container">
-            NovelMenu
+            <ul>
+                <li>목록 가기</li>
+                <li>이전화 보기</li>
+                <li>다음화 보기</li>
+                <li>즐겨찾기</li>
+                <li>맨 위로</li>
+            </ul>
         </article>
     )
 }
