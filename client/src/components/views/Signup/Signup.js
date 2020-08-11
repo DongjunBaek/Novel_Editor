292d20a645cf7d32c916c994bd4793f30107cf4e
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-08-11 21:50:27 +0900

    20200809_novelEditor_구조만들기
    Update Signup.js
    
    회원가입페이지 작성중

--- client/src/components/views/Signup/Signup.js
+++ client/src/components/views/Signup/Signup.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import '../Signup/Signup.css';
 
 function Signup() {
+
+
+
+
     return (
-        <div>
-            Signup
-        </div>
+        <section className="signup-section">
+            <article className="signup-article">
+                <div className="signup-container">
+                    <from className="signup-form">
+                        <div className="signup-form-input">
+                            <ul>
+                                <li>Email : <input type="text" /> </li>
+                                <li>Name : <input type="text" /></li>
+                                <li>password  : <input type="password" /></li>
+                                <li>password Check  : <input type="password" /></li>
+                            </ul>
+                        </div>
+                        <div className="signup-form-preview">
+                            <ul>
+                                <li><img src="" /></li>
+                                <li> Profile Image Upload --> <input type="file" /></li>
+                            </ul>
+                        </div>
+                    </from>
+                </div>
+            </article>
+        </section>
     )
 }
 
