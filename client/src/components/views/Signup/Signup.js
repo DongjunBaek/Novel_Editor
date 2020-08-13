0de98307fd76fab375e91a4477987c2fb241a449
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-08-13 22:36:10 +0900

    Update Signup.js
    회원 가입 view  구조 제작
    
    server로 넘기기 위한 데이터 작업 필요함.

--- client/src/components/views/Signup/Signup.js
+++ client/src/components/views/Signup/Signup.js
@@ -3,35 +3,67 @@ import '../Signup/Signup.css';
 
 function Signup() {
 
-    const [Email, setEmail] = useState('')
+    const [Email, setEmail] = useState('');
+    
+    const [Name, setName] = useState('');
+
+    const [Password, setPassword] = useState('');
+
+    const [PasswordChk, setPasswordChk] = useState('');
+
+
 
     const onEmailHandler = (e) => {
         setEmail(e.target.value);
         console.log(Email)
     };
+    const onNameHandler = (e) => {
+        setName(e.target.value);
+        console.log(Name)
+    };
+    const onPasswordHandler = (e) => {
+        setPassword(e.target.value);
+        console.log(Password)
+    };
+    const onPasswordChkHandler = (e) => {
+        setPasswordChk(e.target.value);
+        console.log(PasswordChk)
+    };
 
+    const onSignUpSumbit = () => {
+        let body = {
+            email : Email,
+            name : Name,
+            password : Password
+        }
+
+        console.log('Input values', body)
+        return false;
+    }
 
 
     return (
         <section className="signup-section">
             <article className="signup-article">
                 <div className="signup-container">
-                    <from className="signup-form">
+                    <form className="signup-form">
                         <div className="signup-form-input">
                             <ul>
                                 <li>Email : <input type="text" onChange={onEmailHandler} /> </li>
-                                <li>Name : <input type="text" /></li>
-                                <li>password  : <input type="password" /></li>
-                                <li>password Check  : <input type="password" /></li>
+                                <li>Name : <input type="text" onChange={onNameHandler} /></li>
+                                <li>password  : <input type="password" onChange={onPasswordHandler} /></li>
+                                <li>password Check  : <input type="password" onChange={onPasswordChkHandler} /></li>
                             </ul>
                         </div>
                         <div className="signup-form-preview">
                             <ul>
                                 <li><img src="" /></li>
                                 <li> Profile Image Upload --> <input type="file" /></li>
+                                <li><button type="button" onClick={onSignUpSumbit}>Sign Up</button></li>
                             </ul>
                         </div>
-                    </from>
+                    
+                    </form>
                 </div>
             </article>
         </section>
