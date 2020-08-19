711c96ea1ef18837178787ed0619a0586b6b6a2e
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-08-19 23:17:03 +0900

    20200819_novelEditor_랜딩페이지 구현중
    랜딩페이지 구현중
    
    글자로 디자인하기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -12,6 +12,13 @@ function LandingPage() {
                 <article className="landing-right">
                     <h2>ovel Editor</h2>
                 </article>
+                <article className="landing-side"> 
+                    <span>소설을 위한 특별한 에디터</span>
+                    <span> 다른 곳에서는 제공하지 않는 특별한 기능</span>
+                    <span> 내 캐릭터 대사에 일러스트 넣기</span>
+                    <span> 나만의 캐릭터 시트지 만들기</span>
+                    <span> 누구나 한번에 간단 사용법</span>
+                </article>
             </div>
     )
 }
