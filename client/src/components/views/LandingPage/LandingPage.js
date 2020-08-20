323de5aea9f61220bbdca21a7192999de0ea9050
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-08-20 23:08:56 +0900

    20200820_novelEditor_랜딩페이지 구현중
    랜딩페이지 디자인 및 구현중
    
    디자인 작업 오래걸리는중

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -13,11 +13,11 @@ function LandingPage() {
                     <h2>ovel Editor</h2>
                 </article>
                 <article className="landing-side"> 
-                    <span>소설을 위한 특별한 에디터</span>
-                    <span> 다른 곳에서는 제공하지 않는 특별한 기능</span>
-                    <span> 내 캐릭터 대사에 일러스트 넣기</span>
-                    <span> 나만의 캐릭터 시트지 만들기</span>
-                    <span> 누구나 한번에 간단 사용법</span>
+                    <span> 1 . 소설을 위한 특별한 에디터</span>
+                    <span> 2 . 다른 곳에서는 제공하지 않는 특별한 기능</span>
+                    <span> 3 . 내 캐릭터 대사에 일러스트 넣기</span>
+                    <span> 4 . 나만의 캐릭터 시트지 만들기</span>
+                    <span> 5 . 누구나 한번에 간단 사용법</span>
                 </article>
             </div>
     )
