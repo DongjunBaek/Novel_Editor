2ca8a6dc1f44783ff36fcdec8d47bbb946d0f90f
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-09-15 22:18:34 +0900

    20200915_novelEditor
    게시판 리스트 페이지
    
    레이아웃변경

--- client/src/components/views/Board/BoardList.js
+++ client/src/components/views/Board/BoardList.js
@@ -6,13 +6,41 @@ function BoardList(props) {
 
     const boardNo = 1;
 
+    const searchBoard = () =>{
+        console.log('Search Board');
+
+    };
+
     return (
         <section className="app main-container border">
-            <article>
+            <article className="boardList-article-container">
+                <div className="boardList-top">
+                    slideShow
+                </div>
                 <div className="boardList-container">
                     <table className="BoardTable">
-                        <thead >                            
-                            <tr  >
+                        <thead>
+                            <tr>
+                                <td>
+                                    <label htmlFor="search">Category</label>
+                                    <select name="search" id="search">
+                                        <option>제목</option>
+                                        <option>작성자</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <input type="text" id="searchBox" placeholder="검색어를 입력해 주세요." />
+                                </td>
+                                <td>
+                                    <button onClick={searchBoard}>Search</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
                                 <th>번호</th>
                                 <th>제목</th>
                                 <th>작성자</th>
@@ -20,18 +48,15 @@ function BoardList(props) {
                                 <th>조회 수</th>
                                 <th>추천 수</th>
                             </tr>
-                        </thead>
-                        <tbody>
-                            
-                                <tr >
-                                    <td>1</td>
-                                    <td><Link to="/boardDetail">Title</Link></td>
-                                    <td>writer</td>
-                                    <td>20-07-31</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                            
+                            <tr >
+                                <td>1</td>
+                                <td><Link to="/boardDetail">Title</Link></td>
+                                <td>writer</td>
+                                <td>20-07-31</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+
                             <tr>
                                 <td>1</td>
                                 <td>Title</td>
@@ -61,6 +86,9 @@ function BoardList(props) {
 
                         </tfoot>
                     </table>
+                </div>
+                <div className="boardList-bot">
+
                 </div>
             </article>
         </section>
