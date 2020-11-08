edc4bd20aded56e6f0770d5b2c2ac1c3b60271a0
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-11-08 23:00:32 +0900

    2020.11.08. landing page css

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import landing_img from '../../../../src/images/bg.jpg';
 // import '../LandingPage/LandingPage.css';
 
 function LandingPage() {
     return (
-        <section className="content-container">
+        <section className="content-container"> 
+            <img id="landing-img"  src={landing_img} />
             <article id="landing">
                 <div>
                     <h1>
